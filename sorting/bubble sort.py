1b130738eeced9621c33a5d9af5aebafc0e5b0e7
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-29 10:06:50 +0530

    add all the code

--- sorting/bubble sort.py
+++ sorting/bubble sort.py
@@ -9,9 +9,9 @@ for j in range(0,n-1+1,1):
         print(a)
     print("")
 
-for i in range(0,n-2+1,1):
-        if a[i]>a[i+1]:
-            c=a[i]
-            a[i]=a[i+1]
-            a[i+1]=c
-        print(a)
+# for i in range(0,n-2+1,1):
+#         if a[i]>a[i+1]:
+#             c=a[i]
+#             a[i]=a[i+1]
+#             a[i+1]=c
+#         print(a)
